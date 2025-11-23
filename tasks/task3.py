50c15dbe2b6155ccf575e641b44bf6675c17ab45
Safarov Arthur <ttttwobata@gmail.com>
2025-11-24 00:18:49 +0500

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,8 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+     word1, word2 = input().split()
+     print("awesome" in (word1, word2))
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
